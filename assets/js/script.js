2b3a7a8fb7d8b26910ef74c6850af564bde9fb0e
Robin <rrlangton@gmail.com>
2024-03-13 23:23:42 -0500

    added icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,127 @@
 const APIKey = "6ba9f7d253ab4329a9862198b61e1a8e";
+const resultContentEl = document.querySelector('#result-content');
+const searchFormEl = document.querySelector('#search-form');
 
 function apiRequest() {
   const city = "Chicago";
   const queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=$Chiacgo&appid=$6ba9f7d253ab4329a9862198b61e1a8e`;
 
-  fetch(queryURL)
-    .then(response => response.json())
-    .then(data => {
-      console.log('Fetch Response \n-------------');
-      console.log(data);
-    })
-    .catch(error => console.error('Error:', error));
-
-  const xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      console.log('XMLHttpRequest Response \n-------------');
-      console.log(xhr.response);
-    }
-  };
-  xhr.open('GET', queryURL);
-  xhr.send();
-
-  $.ajax({
-    url: queryURL,
-    method: 'GET',
-    success: function (response) {
-      console.log('Ajax Response \n-------------');
-      console.log(response);
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-      console.error('Ajax Error:', textStatus, errorThrown);
-    }
-  });
-}
-
-apiRequest();
-
-document.addEventListener("DOMContentLoaded", function () {
-  const searchForm = document.getElementById("searchForm");
-  const searchInput = document.getElementById("searchInput");
-
-  searchForm.addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent form submission
-
-    const userInput = searchInput.value.trim(); // Get user input
-
-    if (userInput !== "") {
-      // Check if input is not empty
-      saveToLocalStorage(userInput); // Save input to local storage
-      console.log("User input saved to local storage:", userInput);
-    } else {
-      console.log("Please enter a city name.");
-    }
-  });
-
-  function saveToLocalStorage(input) {
-    // Save user input to local storage
-    localStorage.setItem("userInput", input);
+  function getParams() {
+    const searchParamsArr = document.location.search.split('&');
+  
+    // Get the query and format values
+    const query = searchParamsArr[0].split('=').pop();
+    const format = searchParamsArr[1].split('=').pop();
+  
+    searchApi(query, format);
   }
-});
+  
+  function printResults(resultObj) {
+    console.log(resultObj);
+  
+    // set up `<div>` to hold result content
+    const resultCard = document.createElement('div');
+    resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
+  
+    const resultBody = document.createElement('div');
+    resultBody.classList.add('card-body');
+    resultCard.append(resultBody);
+  
+    const titleEl = document.createElement('h3');
+    titleEl.textContent = resultObj.title;
+  
+    const bodyContentEl = document.createElement('p');
+    bodyContentEl.innerHTML =
+      `<strong>Date:</strong>${resultObj.date}<br/>`;
+  
+    if (resultObj.subject) {
+      bodyContentEl.innerHTML +=
+        `<strong>Subjects:</strong>${resultObj.subject.join(', ')}<br/>`;
+    } else {
+      bodyContentEl.innerHTML +=
+        '<strong>Subjects:</strong> No subject for this entry.';
+    }
+  
+    if (resultObj.description) {
+      bodyContentEl.innerHTML +=
+        `<strong>Description:</strong>${resultObj.description[0]}`;
+    } else {
+      bodyContentEl.innerHTML +=
+        '<strong>Description:</strong>  No description for this entry.';
+    }
+  
+    const linkButtonEl = document.createElement('a');
+    linkButtonEl.textContent = 'Read More';
+    linkButtonEl.setAttribute('href', resultObj.url);
+    linkButtonEl.classList.add('btn', 'btn-dark');
+  
+    resultBody.append(titleEl, bodyContentEl, linkButtonEl);
+  
+    resultContentEl.append(resultCard);
+  }
+  
+  function searchApi(query, format) {
+    let locQueryUrl = 'https://api.openweathermap.org/search/?fo=json';
+  
+    if (format) {
+      locQueryUrl = `https://api.openweathermap.org/${format}/?fo=json`;
+    }
+  
+    locQueryUrl = `${locQueryUrl}&q=${query}`;
+  
+    fetch(locQueryUrl)
+      .then(function (response) {
+        if (!response.ok) {
+          throw response.json();
+        }
+  
+        return response.json();
+      })
+      .then(function (locRes) {
+        // write query to page so user knows what they are viewing
+        resultTextEl.textContent = locRes.search.query;
+  
+        console.log(locRes);
+  
+        if (!locRes.results.length) {
+          console.log('No results found!');
+          resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
+        } else {
+          resultContentEl.textContent = '';
+          for (let i = 0; i < locRes.results.length; i++) {
+            printResults(locRes.results[i]);
+          }
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }}
+  
+  function handleSearchFormSubmit(event) {
+    event.preventDefault();
+  
+    const searchInputVal = document.querySelector('#search-input').value;
+    const formatInputVal = document.querySelector('#format-input').value;
+  
+    const queryString = `./search-results.html?q=${searchInputVal}&format=${formatInputVal}`;
+  
+    location.assign(queryString);
+  
+    if (!searchInputVal) {
+      console.error('You need a search input value!');
+      return;
+    }
+  
+    getParams();
+  }
+  
+  searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+  
+  getParams();
+
+  get apiRequest();
+  
 
 const APIKey = "6ba9f7d253ab4329a9862198b61e1a8e";
 
